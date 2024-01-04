ee4293b60b9dd603e617d97e6780f8e7445ec005
Dnyaneshwar Vynkati Somvanshi <dnyaneshwar.s@Yash.com>
2024-01-04 19:33:16 +0530

    Lombokn added

--- src/main/java/com/brainqy/swaggerdemo/HelloController.java
+++ src/main/java/com/brainqy/swaggerdemo/HelloController.java
@@ -2,6 +2,7 @@ package com.brainqy.swaggerdemo;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @Api(tags = "Base Api")
+@Slf4j
 public class HelloController {
     @GetMapping("/ui/{user}")
-    @ApiOperation(value = "Get a Greeting message",response = String.class)
-    public String getHello(@PathVariable String user){
-
-        return "Hello " + user ;
+    @ApiOperation(value = "Get a Greeting message", response = String.class)
+    public String getHello(@PathVariable String user) {
+        log.info("I am in Hello controller");
+        return "Hello " + user;
     }
 
 }
